7ba4d490382f27b2e746946f5c3c1e7e62f6efc9
Dev.Nah <mcthundercat99@gmail.com>
2022-04-12 15:00:15 -0400

    initial upload for html

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    // Log 1
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Log 2
-    profileDataArr.forEach((profileItem) => {
-      console.log(profileItem)
-    });
-};
-
-printProfileData(profileDataArgs);
