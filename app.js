b832ac75c13b52c17eaee56caa21018bfc436ac7
Dev.Nah <mcthundercat99@gmail.com>
2022-04-13 22:24:49 -0400

    More Questions

--- app.js
+++ app.js
@@ -1,14 +1,68 @@
 const inquirer = require('inquirer');
 
-inquirer
-  .prompt([
+const promptUser = () => {
+  return inquirer.prompt ([
     {
       type: 'input',
-      name: 'name',
+      name: 'Name',
       message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'GitHub',
+      message: 'Enter GitHub Username'
+    },
+    {
+      type: 'input',
+      name: 'About',
+      message: 'Provide some info about yourself:'
     }
-  ])
-  .then(answers => console.log(answers));
+  ]);
+};
+
+const promptProject = () => {
+  console.log(``);
+  return inquirer.prompt ([
+    {
+      type: 'input',
+      name: 'Name',
+      message: 'What is the name of your project?'
+    },
+    {
+      type: 'input',
+      name: 'Description',
+      message: 'Provide a description of the project (Required)'
+    },
+    {
+      type: 'checkbox',
+      name: 'Languages',
+      message: 'What did you build this project with? (Check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'Link',
+      message: 'Enter the GitHub link to your project. (Required)'
+    },
+    {
+      type: 'confirm',
+      name: 'Feature',
+      message: 'Would you like to feature this project?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: false
+    }
+  ]);
+};
+
+promptUser()
+.then(answers => console.log(answers))
+.then(promptProject)
+.then(projectAnswers => console.log(projectAnswers));
 // const fs = require('fs');
 // const generatePage = require('./src/page-template');
 
